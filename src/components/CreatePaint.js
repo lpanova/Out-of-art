5c1ca821a2448fa7016f859a7c58bb7da1b58618
liliapanova <lilia_panova@abv.bg>
2021-03-07 10:36:05 +0200

    1. Added new CreatePaint component with upload file and create paint functional; 2. Added new Form.css file;

--- src/components/CreatePaint.js
+++ src/components/CreatePaint.js
@@ -1,6 +1,146 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../css/Form.css';
 
 function CreatePaint(props) {
-  return <div>CreatePaint</div>;
+  const [file, setFile] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const author = localStorage.getItem('username');
+
+  const kinveyAppKey = 'kid_S13nVzcMO';
+  const authToken = 'Kinvey ' + localStorage.getItem('authtoken');
+
+  function handleFileChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleDescChange(event) {
+    setDescription(event.target.value);
+  }
+
+  function createMetadata(metadata, appKey, authToken) {
+    return fetch(`https://baas.kinvey.com/blob/${appKey}`, {
+      method: 'POST',
+      Host: 'baas.kinvey.com',
+      headers: {
+        Accept: 'application/json',
+        Authorization: authToken,
+        'Content-Type': 'application/json',
+        'X-Kinvey-Content-Type': file.type
+      },
+      body: JSON.stringify(metadata)
+    });
+  }
+
+  function upload(uploadUrl, headers) {
+    const fileReader = new FileReader();
+    fileReader.onload = function (e) {
+      fetch(uploadUrl, {
+        method: 'PUT',
+        headers: {
+          ...headers
+        },
+        body: e.target.result
+      }).then(function (response) {
+        return response.text();
+      });
+    };
+
+    fileReader.readAsArrayBuffer(file);
+  }
+
+  function createPaint(id, appKey, authToken) {
+    return fetch(`https://baas.kinvey.com/appdata/${appKey}/Paints`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: authToken,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        author,
+        fileImage: {
+          type: 'KinveyFile',
+          _id: id
+        }
+      })
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const metadata = {
+      _filename: file.name,
+      mimeType: file.type,
+      _public: true
+    };
+
+    const resp1 = await createMetadata(metadata, kinveyAppKey, authToken);
+    if (!resp1.ok) {
+      throw new Error('cannot write metadata');
+    }
+
+    const resp1json = await resp1.json();
+
+    upload(resp1json._uploadURL, {
+      ...resp1json._requiredHeaders,
+      'Content-Type': metadata.mimeType
+    });
+
+    const resp3 = await createPaint(resp1json._id, kinveyAppKey, authToken);
+    if (!resp3.ok) {
+      throw new Error('cannot write in paint collection');
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <div className=" full-screen">
+          <div className="mt-6">
+            <h5 className="login-text">Upload</h5>
+          </div>
+          <div className=" flex-x-center full-screen">
+            <form className="mt-6 form" onSubmit={handleSubmit}>
+              <div>Upload file:</div>
+              <input
+                type="file"
+                name="file"
+                onChange={handleFileChange}
+                className="form-input"
+                accept=".jpg, .jpeg, .png"
+              />
+              <input type="submit" value="Upload" className="form-button" />
+
+              <div>Name:</div>
+              <input
+                type="text"
+                name="name"
+                onChange={handleNameChange}
+                className="form-input"
+              />
+
+              <div>Description:</div>
+              <textarea
+                type="text"
+                name="description"
+                onChange={handleDescChange}
+                rows="6"
+                className="form-input"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default CreatePaint;
