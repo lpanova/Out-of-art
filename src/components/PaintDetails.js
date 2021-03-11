b2664ba4d7cdcb78db26a315ddc499618c77fc59
liliapanova <lilia_panova@abv.bg>
2021-03-11 22:08:56 +0200

    Implement delete paint logic.

--- src/components/PaintDetails.js
+++ src/components/PaintDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import '../App.css';
 import '../css/Paint.css';
 import Loading from './Loading';
@@ -11,18 +12,17 @@ function PaintDetails(props) {
     description: '',
     author: ''
   });
+  const [fileId, setFileId] = useState('');
 
   const [loading, setLoading] = useState(true);
 
   const [edit, setEdit] = useState(false);
 
   const user = localStorage.username;
-  console.log(user);
-
+  const history = useHistory();
   const kinveyAppKey = 'kid_S13nVzcMO';
   const authToken = 'Kinvey ' + localStorage.getItem('authtoken');
   const id = props.match.params.id;
-  console.log(id);
 
   function details(kinveyAppKey, authToken, id) {
     return fetch(
@@ -46,11 +46,51 @@ function PaintDetails(props) {
     const resp1json = await resp1.json();
     console.log(resp1json);
     setPaintDetails(resp1json);
+    setFileId(resp1json.fileImage._id);
     setLoading(false);
 
     if (user === resp1json.author) {
       setEdit(true);
     }
+  }
+
+  function delPainObject(kinveyAppKey, authToken, id) {
+    return fetch(
+      `https://baas.kinvey.com/appdata/${kinveyAppKey}/Paints/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Accept: 'application/json',
+          Authorization: authToken,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
+  function delFile(delFile) {
+    return fetch(`https://baas.kinvey.com/blob/${kinveyAppKey}/${fileId}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        Authorization: authToken,
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  async function deletePaint() {
+    const resp1 = await delFile(delFile);
+    if (!resp1.ok) {
+      throw new Error('cannot get paintsData');
+    }
+
+    const resp2 = await delPainObject(kinveyAppKey, authToken, id);
+    if (!resp2.ok) {
+      throw new Error('cannot get paintsData');
+    }
+    history.push('/paints');
+    console.log('success');
   }
 
   useEffect(() => {
@@ -90,6 +130,7 @@ function PaintDetails(props) {
                 Edit
               </Link>
             }
+            <button onClick={deletePaint}>Delete</button>
           </div>
         ) : (
           <div></div>
