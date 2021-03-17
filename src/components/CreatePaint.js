4d1fd7e193a1ee6f49fd380c485529ccb7726c01
liliapanova <lilia_panova@abv.bg>
2021-03-17 23:04:55 +0200

    Add validation logic in create paint page.

--- src/components/CreatePaint.js
+++ src/components/CreatePaint.js
@@ -1,25 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import '../App.css';
 import '../css/Form.css';
+import { getAuthenticationToken, kinveyAppKey } from '../utils/kinvey';
+import { useHistory } from 'react-router-dom';
 
 function CreatePaint() {
+  let history = useHistory();
   const [file, setFile] = useState(null);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const author = localStorage.getItem('username');
   const [likes] = useState(['like']);
 
-  const kinveyAppKey = 'kid_S13nVzcMO';
-  const authToken = 'Kinvey ' + localStorage.getItem('authtoken');
+  const [fileError, setFileError] = useState({
+    message: ''
+  });
+
+  const [nameError, setNameError] = useState({
+    message: ''
+  });
+
+  const [descriptionError, setDescriptionError] = useState({
+    message: ''
+  });
+
+  const inputFileFocus = useRef(null);
+  const inputNameFocus = useRef(null);
+  const inputDescriptionFocus = useRef(null);
+
+  function clearFileError() {
+    inputFileFocus.current.focus();
+    setFileError({
+      message: ''
+    });
+  }
+
+  function clearNameError() {
+    inputNameFocus.current.focus();
+    setNameError({
+      message: ''
+    });
+  }
+
+  function clearDescriptionError() {
+    inputDescriptionFocus.current.focus();
+    setDescriptionError({
+      message: ''
+    });
+  }
 
   function handleFileChange(event) {
     setFile(event.target.files[0]);
   }
-
   function handleNameChange(event) {
     setName(event.target.value);
   }
-
   function handleDescChange(event) {
     setDescription(event.target.value);
   }
@@ -79,27 +114,68 @@ function CreatePaint() {
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const metadata = {
-      _filename: file.name,
-      mimeType: file.type,
-      _public: true
-    };
+    if (!file) {
+      setFileError({
+        message: 'Please select image.'
+      });
+    } else if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
+      setFileError({
+        message: 'Please select valid image format.'
+      });
+    } else if (file) {
+      const size = parseFloat(file.size / (1024 * 1024)).toFixed(2);
+      if (size > 2) {
+        setFileError({
+          message: 'Please select image size less than 2 MB'
+        });
+      } else {
+        const metadata = {
+          _filename: file.name,
+          mimeType: file.type,
+          _public: true
+        };
 
-    const resp1 = await createMetadata(metadata, kinveyAppKey, authToken);
-    if (!resp1.ok) {
-      throw new Error('cannot write metadata');
-    }
+        const resp1 = await createMetadata(
+          metadata,
+          kinveyAppKey,
+          getAuthenticationToken()
+        );
+        if (!resp1.ok) {
+          throw new Error('cannot write metadata');
+        }
 
-    const resp1json = await resp1.json();
+        const resp1json = await resp1.json();
 
-    upload(resp1json._uploadURL, {
-      ...resp1json._requiredHeaders,
-      'Content-Type': metadata.mimeType
-    });
+        upload(resp1json._uploadURL, {
+          ...resp1json._requiredHeaders,
+          'Content-Type': metadata.mimeType
+        });
 
-    const resp3 = await createPaint(resp1json._id, kinveyAppKey, authToken);
-    if (!resp3.ok) {
-      throw new Error('cannot write in paint collection');
+        if (name.length === 0) {
+          setNameError({
+            message: 'Name is required.'
+          });
+        } else if (name.length > 30) {
+          setNameError({
+            message: 'Name must be less than 30 characters.'
+          });
+        }
+        if (description.length > 50) {
+          setDescriptionError({
+            message: 'Description must be less than 50 characters.'
+          });
+        } else {
+          const resp3 = await createPaint(
+            resp1json._id,
+            kinveyAppKey,
+            getAuthenticationToken()
+          );
+          history.push('/mypaints');
+          if (!resp3.ok) {
+            throw new Error('cannot write in paint collection');
+          }
+        }
+      }
     }
   }
 
@@ -113,34 +189,43 @@ function CreatePaint() {
 
           <form className="form" onSubmit={handleSubmit}>
             <div className="flex-x-center">
-              <div className="wrapper-input">
+              <div className="wrapper-input" onClick={clearFileError}>
                 <div className="text-center m1">Upload file:</div>
                 <input
                   type="file"
                   name="file"
                   onChange={handleFileChange}
-                  accept=".jpg, .jpeg, .png"
+                  ref={inputFileFocus}
                   className="formelement wrapper-input"
                 />
+                <p className="error-message">{fileError.message}</p>
               </div>
             </div>
 
-            <div>Name:</div>
-            <input
-              type="text"
-              name="name"
-              onChange={handleNameChange}
-              className="form-input"
-            />
+            <div onClick={clearNameError}>
+              <div>Name:</div>
+              <input
+                type="text"
+                name="name"
+                onChange={handleNameChange}
+                ref={inputNameFocus}
+                className="form-input"
+              />
+              <p className="error-message">{nameError.message}</p>
+            </div>
+            <div onClick={clearDescriptionError}>
+              <div>Description:</div>
+              <textarea
+                type="text"
+                name="description"
+                onChange={handleDescChange}
+                rows="6"
+                ref={inputDescriptionFocus}
+                className="form-input"
+              />
+              <p className="error-message">{descriptionError.message}</p>
+            </div>
 
-            <div>Description:</div>
-            <textarea
-              type="text"
-              name="description"
-              onChange={handleDescChange}
-              rows="6"
-              className="form-input"
-            />
             <input type="submit" value="Submit" className="form-button " />
           </form>
         </div>
